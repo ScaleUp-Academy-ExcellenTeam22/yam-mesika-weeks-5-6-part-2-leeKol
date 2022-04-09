f9ad6ee345a8b30117c64938d42a8159037ef26e
Lee Kol <lee.levy333@gmail.com>
2022-04-09 14:49:03 +0300

    The a_perfect_dish_to_share exercise is done.

---
+++ a_perfect_dish_to_share.py
@@ -0,0 +1,23 @@
+from typing import Generator
+
+
+def perfect_dish_size_to_share() -> Generator[int, None, None]:
+    """
+        The function is a generator that infinitely returns sizes of dishes that are considered perfect for share,
+        i.e. a dish that if we sum up all the ways to divide it, we get the size of the dish itself.
+        :return: The size of a "perfect for share" dish.
+    """
+    current_dish_size = 2
+    while True:
+        divisor_sum = 0
+        for number in range(1, int((current_dish_size / 2) + 1)):
+            if current_dish_size % number == 0:
+                divisor_sum += number
+        if divisor_sum == current_dish_size:
+            yield current_dish_size
+        current_dish_size += 1
+
+
+if __name__ == '__main__':
+    for dish_size in perfect_dish_size_to_share():
+        print(dish_size)
