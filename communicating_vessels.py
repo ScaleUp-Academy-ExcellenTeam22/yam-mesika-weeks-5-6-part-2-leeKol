d42de8c7b69c6e04e62994340abe6a5c55396456
Lee Kol <lee.levy333@gmail.com>
2022-04-09 22:43:39 +0300

    The communicating_vessels exercise is done.

---
+++ communicating_vessels.py
@@ -0,0 +1,17 @@
+from collections.abc import Collection
+import itertools
+
+
+def interleave(*iterables: Collection) -> list:
+    """
+        The function receives one or more iterable parameters, and returns a list of intertwined items.
+        :param iterables: The iterables that need to be intertwined.
+        :return: The intertwined list.
+    """
+    return list(filter(lambda item: item is not None, list(itertools.chain(*list(itertools.zip_longest(*iterables))))))
+
+
+if __name__ == '__main__':
+    interleave_generator = (list_item for list_item in interleave('abcde', [1, 2, 3], ('!', '@', '#')))  # the generator
+    interwoven_list = list(interleave_generator)
+    print(interwoven_list)
